19bd36a54ebb028c5a3e414d5f958b3e662c7654
axlc <63615267+axlc@users.noreply.github.com>
2020-08-08 21:56:43 -0400

    Add files via upload

---
+++ cloned_git_repos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script will find all git repositories on your system and output the results to a file in the user's home directory
+
+CUR_DIR=$(pwd)
+
+echo -e "\n\033[1mSearchign for all git repositories on your system...\033[0m\n"
+
+for d in $(find / -name .git -type d 2>/dev/null); do 
+    cd $d/..
+    git remote -v >> ~/remote.txt
+done
+
+expand -t 1 ~/remote.txt | tr -s ' ' | cut -d ' ' -f 2 | sort | uniq 
+
+cd $CUR_DIR
+
+echo -e "\n\033[32mDone!\033[0m\n"                                                                                 
+
